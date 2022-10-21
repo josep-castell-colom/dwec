3836ad9d17a2b529ab2b726b1a25b114388c4b43
josep <jcastellcolom@cifpfbmoll.eu>
2022-10-21 12:39:23 +0200

    added zodiac object array

--- programacion-avanzada/main.js
+++ programacion-avanzada/main.js
@@ -134,6 +134,99 @@ const zodiacInput = document.getElementById("zodiaco-input");
 const zodiacButton = document.getElementById("zodiaco-button");
 const zodiacOutput = document.getElementById("zodiaco-output");
 
-const signos = new Map([
-  [signo => "Acuario", inicio => Date(), fin => Date()]
-])
+const signos = [
+  {
+    name: "Aquario",
+    inicio: new Date(2022, 00, 21),
+    fin: new Date(2022, 01, 19),
+    planeta: "Urano",
+    elemento: "Aire"
+  },
+  {
+    name: "Piscis",
+    inicio: new Date(2022, 01, 20),
+    fin: new Date(2022, 02, 20),
+    planeta: "Neptuno",
+    elemento: "Agua"
+  },
+  {
+    name: "Aries",
+    inicio: new Date(2022, 02, 21),
+    fin: new Date(2022, 03, 20),
+    planeta: "Marte",
+    elemento: "Fuego"
+  },
+  {
+    name: "Tauro",
+    inicio: new Date(2022, 03, 21),
+    fin: new Date(2022, 04, 21),
+    planeta: "Venus",
+    elemento: "Tierra"
+  },
+  {
+    name: "Géminis",
+    inicio: new Date(2022, 04, 22),
+    fin: new Date(2022, 05, 21),
+    planeta: "Mercurio",
+    elemento: "Aire"
+  },
+  {
+    name: "Cáncer",
+    inicio: new Date(2022, 05, 22),
+    fin: new Date(2022, 06, 23),
+    planeta: "Luna",
+    elemento: "Agua"
+  },
+  {
+    name: "Leo",
+    inicio: new Date(2022, 06, 24),
+    fin: new Date(2022, 07, 23),
+    planeta: "Sol",
+    elemento: "Fuego"
+  },
+  {
+    name: "Virgo",
+    inicio: new Date(2022, 07, 24),
+    fin: new Date(2022, 08, 23),
+    planeta: "Mercurio",
+    elemento: "Tierra"
+  },
+  {
+    name: "Libra",
+    inicio: new Date(2022, 08, 24),
+    fin: new Date(2022, 09, 23),
+    planeta: "Venus",
+    elemento: "Aire"
+  },
+  {
+    name: "Escorpio",
+    inicio: new Date(2022, 09, 24),
+    fin: new Date(2022, 10, 22),
+    planeta: "Marte y Plutón",
+    elemento: "Agua"
+  },
+  {
+    name: "Sagitario",
+    inicio: new Date(2022, 10, 23),
+    fin: new Date(2022, 11, 22),
+    planeta: "Júpiter",
+    elemento: "Fuego"
+  },{
+    name: "Capricornio",
+    inicio: new Date(2022, 11, 23),
+    fin: new Date(2022, 00, 20),
+    planeta: "Saturno",
+    elemento: "Tierra"
+  },
+]
+
+let fechaTest = new Date(1988, 09, 08);
+
+console.log(fechaTest);
+
+signos.map(({name, inicio, fin, planeta, elemento}) => {
+  if ((fechaTest.getMonth() == inicio.getMonth() && fechaTest.getDay() >= inicio.getDay()) || 
+        (fechaTest.getMonth() == fin.getMonth() && fechaTest.getDay() <= fin.getDay())){
+    console.log(name);
+  }
+})
